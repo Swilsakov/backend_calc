b191b5e2fd7f857303b053ec73fae4bc93436773
Swilsakov <shkodaonebob@gmail.com>
2023-05-24 15:11:34 +0600

    some fix

--- middleware/checkSession.js
+++ middleware/checkSession.js
@@ -1,25 +1,37 @@
 const jwt = require("jsonwebtoken");
+const db = require("../db");
 
-const checkSessionExpiration = (req, res, next) => {
-  if (req.method === "OPTIONS") {
-    return next();
-  }
-
-  const token = req.headers.authorization;
-  if (!token) {
-    return res.status(401).json({ message: "No token provided" });
-  }
-
-  jwt.verify(token, "PERN", (err, decoded) => {
-    if (err) {
-      if (err.name === "TokenExpiredError") {
-        return res.status(401).json({ message: "Token expired" });
-      }
-      return res.status(401).json({ message: "Invalid token" });
+const checkSessionExpiration = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    if (!token) {
+      return res.status(401).json({ message: "No token provided" });
     }
-    req.userId = decoded.id;
+
+    const decoded = jwt.verify(token, "PERN");
+    req.user = decoded;
+
+    const foundUser = await db.query("SELECT * FROM users WHERE id = $1", [
+      req.user.id,
+    ]);
+
+    if (foundUser.rows.length === 0) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    req.foundUser = foundUser.rows[0]; // Добавляем foundUser в req
+
     next();
-  });
+  } catch (error) {
+    if (error instanceof jwt.TokenExpiredError) {
+      return res.status(401).json({ message: "Token expired" });
+    } else if (error instanceof jwt.JsonWebTokenError) {
+      return res.status(401).json({ message: "Invalid token" });
+    } else {
+      console.log(error);
+      return res.status(500).json({ message: "Server error" });
+    }
+  }
 };
 
 module.exports = checkSessionExpiration;
--- routes/auth.router.js
+++ routes/auth.router.js
@@ -5,13 +5,9 @@ const checkSessionExpiration = require("../middleware/checkSession");
 
 router.post("/registration", authController.registration);
 router.post("/login", authController.login);
-// router.get("/protected", checkSessionExpiration, (req, res) => {
-//   // Защищенный маршрут, доступный только при активной сессии
-//   res.status(200).json({ message: "Protected route" });
-// });
-
 router.get("/protected", checkSessionExpiration, (req, res) => {
-  res.status(200).json({ message: "Protected route" });
+  // Защищенный маршрут, доступный только при активной сессии
+  res.status(200).json({ message: "Protected route", user: req.foundUser });
 });
 
 module.exports = router;
